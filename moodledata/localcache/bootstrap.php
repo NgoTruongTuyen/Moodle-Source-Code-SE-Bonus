60c93474b177d4de5d2a49bb4806d7e787ff06dd
NgoTruongTuyen <ngotuyen781@gmail.com>
2022-01-12 17:58:12 +0700

    add moodle source

---
+++ moodledata/localcache/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+// ********** This file is generated DO NOT EDIT **********
+$CFG->siteidentifier = 'ZxnDQsQgP5aNbESM3Z0JXY7TOCnHYmFUlocalhost';
+$CFG->bootstraphash = '7b2955eac51624188b1ec1bd77cc6561';
+// Only if the file is not stale and has not been defined.
+if ($CFG->bootstraphash === hash_local_config_cache() && !defined('SYSCONTEXTID')) {
+    define('SYSCONTEXTID', 1);
+}
